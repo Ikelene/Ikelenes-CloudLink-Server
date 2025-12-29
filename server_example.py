4783b2c086d9ba437054221b07114b6857d0d222
[Ikelene] <christopherh2480@gmail.com>
2025-12-29 08:53:16 -0700

    change ip so github accepts it

--- server_example.py
+++ server_example.py
@@ -89,4 +89,5 @@ if __name__ == "__main__":
     # server.enable_ssl(certfile="cert.pem", keyfile="privkey.pem")
     
     # Start the server
-    server.run(ip="ikelene.github.io", port=3000)
+    server.run(ip="0.0.0.0", port=3000)
+
